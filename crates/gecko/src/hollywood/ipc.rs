6b7fa633ad89ba64d7efac9955238d80c14d279b
Layle <me@layle.dev>
2026-04-29 11:21:24 +0200

    forgot to trace IPC r/w

--- crates/gecko/src/hollywood/ipc.rs
+++ crates/gecko/src/hollywood/ipc.rs
@@ -1,11 +1,18 @@
 use crate::system::{System, SystemId};
 
 #[inline(always)]
-pub fn ipc_read<const SYSTEM: SystemId>(_sys: &mut System<SYSTEM>, _addr: u32, _size: u32) -> Option<u32> {
+pub fn ipc_read<const SYSTEM: SystemId>(_sys: &mut System<SYSTEM>, addr: u32, size: u32) -> Option<u32> {
+    tracing::error!("unhandled IPC read at {:#010X} (size {})", addr, size);
     Some(0)
 }
 
 #[inline(always)]
-pub fn ipc_write<const SYSTEM: SystemId>(_sys: &mut System<SYSTEM>, _addr: u32, _size: u32, _val: u32) -> bool {
+pub fn ipc_write<const SYSTEM: SystemId>(_sys: &mut System<SYSTEM>, addr: u32, size: u32, val: u32) -> bool {
+    tracing::error!(
+        "unhandled IPC write at {:#010X} (size {}, value {:#010X})",
+        addr,
+        size,
+        val
+    );
     true
 }
